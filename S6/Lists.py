1ca11c5d3c08e9751379e6e0bed1e9a1a5ace7f2
Norbert Dozsa <dozsanorbert@hotmail.com>
2023-02-22 21:15:58 +0200

    S6

---
+++ S6/Lists.py
@@ -0,0 +1,15 @@
+lista_goala = []
+# alfabet = range(64, 91, 2)
+
+# ASCII LIST
+
+# for i in alfabet:
+#     lista_goala.append(chr(i))
+
+
+# print(lista_goala)
+
+for i in range(65, 91):
+    lista_goala.append(chr(i))
+
+print(lista_goala)
